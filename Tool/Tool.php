6fb7e28a6911ac2b6723ae7799cc934111c4d5e4
zaire <zhangjh@sootoo.com>
2019-09-27 11:10:22 +0800

    init

---
+++ Tool/Tool.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/9/27
+ * Time: 11:07
+ */
+
+namespace Tool;
+
+class Tool {
+
+    public function __construct()
+    {
+    }
+
+    public static function hello()
+    {
+        echo 'Hello World';
+    }
+}
